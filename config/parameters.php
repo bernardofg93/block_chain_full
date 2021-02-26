6e761af36a42a5afff66de77ce7c26eb47735866
bernardo <bernardo930321@gmail.com>
2021-02-25 17:17:16 -0800

    -v 1.0.0

---
+++ config/parameters.php
@@ -0,0 +1,5 @@
+<?php
+
+define("BASE_URL", "http://localhost/block_chain/");
+define("controller_default", "pageController");
+define("action_default", "home");
---
+++ views/layout/footer.php
@@ -0,0 +1,45 @@
+<footer>
+    <ul class="footer-1">
+        <ul class="social-app">
+            <li>
+                <h2>Contact Info</h2>
+                <i class="fab fa-facebook-f"></i>
+                <i class="fab fa-twitter"></i>
+                <i class="fab fa-instagram"></i>
+            </li>
+        </ul>
+        <li>
+            <i class="fas fa-phone-volume"></i>
+            <p>
+                USA +1 (619)-721-1973   
+                +1 (619)-395-5116
+            </p>
+        </li>
+        <li>
+            <i class="fas fa-map-marked-alt"></i>
+            <p>
+                9635 Heinrich Hertz drive , 
+                suite 4 ,San Diego CA, 92154
+            </p>
+        </li>
+        <li>
+            <p id="copy">&copy;</p><p>Copyright Blockchain Logistics</p>
+        </li>
+    </ul>
+    <ul class="footer-2">
+        <li class="box-2">
+            <p>
+                <i class="far fa-envelope"></i>
+                logistics@blchainlogistics.com
+            </p>
+            <p>management@blchainlogistics.com</p>
+            <h4 href="">Newsletter</h4>
+            <form action="">
+                <input type="email" name="" id="" placeholder="Email*">
+                <input type="submit" value="Send">
+            </form>
+        </li>
+    </ul>
+</footer>
+</body>
+</html>
