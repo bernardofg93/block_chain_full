750414076aec95d1433cdc9f9968555127d346c1
bernardofg93 <61402193+bernardofg93@users.noreply.github.com>
2021-02-25 17:27:24 -0800

    Update parameters.php

--- config/parameters.php
+++ config/parameters.php
@@ -1,5 +1,5 @@
 <?php
 
-define("BASE_URL", "http://localhost/block_chain/");
+define("BASE_URL", "https://blockc.herokuapp.com/");
 define("controller_default", "pageController");
 define("action_default", "home");
